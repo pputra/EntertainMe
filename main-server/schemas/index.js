16f2ea08dca1f0e2606febbe4eaf51e770870db2
pputra <mynameisyoga@live.com>
2018-11-13 12:10:29 +0700

    implement add data

--- main-server/schemas/index.js
+++ main-server/schemas/index.js
@@ -1,7 +1,8 @@
-const { GraphQLSchema, GraphQLObjectType, GraphQLList } = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLString, GraphQLInt } = require('graphql');
 const MovieType = require('./Movie');
 const SeriesType =require('./Series');
 const { getMovies, getSeries } = require('../controllers/main');
+const axios = require('axios');
 
 const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
@@ -19,6 +20,63 @@ const schema = new GraphQLSchema({
                 resolve: async () => {
                     let movies = await getSeries();
                     return movies.data;
+                }
+            }
+        }
+    }),
+    mutation: new GraphQLObjectType({
+        name: 'mutation',
+        fields: {
+            addMovie: {
+                type: MovieType,
+                args: {
+                    title: { type: GraphQLString },
+                    overview: { type: GraphQLString},
+                    tag: {type: new GraphQLList( GraphQLString ) },
+                    poster_path: { type: GraphQLString },
+                    popularity: { type: GraphQLInt },
+                    createdAt: { type: GraphQLString },
+                    updatedAt: { type: GraphQLString }
+                },
+                resolve: async (_parent, args, _context) => {
+                    
+
+                    
+
+                    let response = await axios({
+                            method: 'POST',
+                            url: 'http://localhost:3001/movie',
+                            data: args,
+                        });
+
+                    
+                    return response.data;
+                }
+            },
+            addSeries: {
+                type: MovieType,
+                args: {
+                    title: { type: GraphQLString },
+                    overview: { type: GraphQLString},
+                    tag: {type: new GraphQLList( GraphQLString ) },
+                    poster_path: { type: GraphQLString },
+                    popularity: { type: GraphQLInt },
+                    createdAt: { type: GraphQLString },
+                    updatedAt: { type: GraphQLString }
+                },
+                resolve: async (_parent, args, _context) => {
+                    
+
+                    
+
+                    let response = await axios({
+                            method: 'POST',
+                            url: 'http://localhost:3002/tv',
+                            data: args,
+                        });
+
+                    
+                    return response.data;
                 }
             }
         }
